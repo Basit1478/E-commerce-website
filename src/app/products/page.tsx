e1616f1c7e6025c9a390eade90589d3508e5cfc1
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-01-04 12:51:15 +0500

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import React from "react";
 import Link from "next/link";
-const page = () => {
+const Page = () => {
    const [isOpen, setIsOpen] = useState(false);
       
         const toggleMenu = () => {
