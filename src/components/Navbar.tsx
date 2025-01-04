28a0fe3d9b630e9beb4834b38f522bdbd2f9bc48
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-01-04 12:34:08 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
-"use client";
-
-import React, { useState } from "react";
-
+'use client'
+import { useState } from "react";
+import React from 'react';
+import Link from "next/link";
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsOpen(!isOpen);
   };
-  
+
   return (
-<div>
+    <div>
     <header className="max-w-[1440px] mx-auto md:h-[66px] md:px-10 px-5 sticky top-0 md:static backdrop-blur-sm z-20">
   <div className="md:border-b-[1px] border-[#0000004f] w-full mx-auto flex h-[66px] justify-between items-center md:pb-2">
     <div className="absolute hidden md:block  top-16 md:top-0 md:left-20  md:bg-white rounded-md p-2 transition-all duration-300 ease-in-out transform -translate-y-12 opacity-0">
@@ -135,56 +135,160 @@ const Navbar = () => {
           />
         </svg>
       </div>
-      <div className="md:hidden z-10 h-6 w-6">
-        <button
-          type="button"
-          aria-haspopup="dialog"
-          aria-expanded="false"
-          aria-controls="radix-:Rq5kq:"
-          data-state="closed"
+ 
+ {/* Mobile Menu */}
+       {/* Menu Button */}
+       <button
+        onClick={toggleMenu}
+        className="md:hidden z-10 h-6 w-6 flex justify-center items-center"
+        aria-haspopup="dialog"
+        aria-expanded={isOpen}
+        aria-controls="menu-dialog"
+      >
+        <svg
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={0}
+          viewBox="0 0 15 15"
+          className="h-full w-full"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            stroke="currentColor"
-            fill="none"
-            strokeWidth={0}
-            viewBox="0 0 15 15"
-            className="h-full w-full"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+
+      {/* Menu Drawer */}
+      {isOpen && (
+        <div
+          role="dialog"
+          id="menu-dialog"
+          aria-labelledby="menu-title"
+          aria-describedby="menu-description"
+          data-state={isOpen ? "open" : "closed"}
+          className="fixed z-50 inset-y-0 right-0 bg-white w-3/4 max-w-sm shadow-lg transition-transform ease-in-out duration-500 transform translate-x-0 border-l h-full"
+        >
+          {/* Close Button */}
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="absolute right-4 top-4 rounded-sm opacity-70 hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
           >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
-              fill="currentColor"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-x h-6 w-6"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </button>
+
+          {/* Menu Content */}
+          <div className="p-6 bg-white">
+            <h2
+              id="menu-title"
+              className="text-2xl text-[#22202E] font-semibold text-center mb-4"
+            >
+              <Link href="/">Avion</Link>
+            </h2>
+
+            <nav className="flex flex-col space-y-4 text-center font-light ">
+            <a
+        className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+        href="/"
+      >
+        Home
+      </a>
+      <a
+        className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+        href="/products"
+      >
+        All Products
+      </a>
+      <a
+        className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+        href="/about-us"
+      >
+        About Us
+      </a>
+      <a
+      className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+      href="#ceramics"
+    >
+      Ceramics
+    </a>
+    <a
+      className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+      href="#tables"
+    >
+      Tables
+    </a>
+    <a
+      className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+      href="#chairs"
+    >
+      Chairs
+    </a>
+    <a
+      className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+      href="#tableware"
+    >
+      Tableware
+    </a>
+    <a
+      className="hover:text-[#5a526c] border-b-1 border-transparent hover:border-[#5a526c] pb-1"
+      href="#cutlery"
+    >
+      Cutlery
+    </a>
+            </nav>
+
+            <div className="mt-6">
+              <button className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+        </div>
+
   </div>
 </header>
 <nav className="sticky top-0 z-50 mb-8 w-full mt-2 bg-white h-[66px] xl:gap-x-11 md:gap-x-8 text-[#726E8D] text-[16px] hidden md:flex justify-center items-center ">
+    {/* Menu for Desktop */}
+    <div className="hidden md:flex space-x-8 text-gray-600">
     <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/"
-    >
-      Home
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/products"
-    >
-      All Products
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/about-us"
-    >
-      About Us
-    </a>
-    <a
+        className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
+        href="/"
+      >
+        Home
+      </a>
+      <a
+        className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
+        href="/products"
+      >
+        All Products
+      </a>
+      <a
+        className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
+        href="/about-us"
+      >
+        About Us
+      </a>
+      <a
       className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
       href="#ceramics"
     >
@@ -214,93 +318,10 @@ const Navbar = () => {
     >
       Cutlery
     </a>
-        {/* Hamburger Menu Icon for Mobile */}
-          <div className="md:hidden">
-            <button
-              onClick={toggleMenu}
-              className="text-orange-500 hover:text-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-800"
-            >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d={
-                    isMenuOpen
-                      ? "M6 18L18 6M6 6l12 12"
-                      : "M4 6h16M4 12h16M4 18h16"
-                  }
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile Menu */}
-      {isMenuOpen && (
-        <div className="md:hidden bg-white">
-          <div className="px-2 pt-2 pb-3 space-y-1">
-             className="sticky top-0 z-50 mb-8 w-full mt-2 bg-white h-[66px] xl:gap-x-11 md:gap-x-8 text-[#726E8D] text-[16px] hidden md:flex justify-center items-center ">
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/"
-    >
-      Home
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/products"
-    >
-      All Products
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="/about-us"
-    >
-      About Us
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="#ceramics"
-    >
-      Ceramics
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="#tables"
-    >
-      Tables
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="#chairs"
-    >
-      Chairs
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      href="#tableware"
-    >
-      Tableware
-    </a>
-    <a
-      className="hover:text-[#5a526c] border-b-2 border-transparent hover:border-[#5a526c] pb-1"
-      >
-      Cutlery 
-    </a>
-            
-            
-          </div>
-        </div>
+       </div>
   </nav>
-   </>
+ </div>
+
   );
 };
 
